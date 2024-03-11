8fa392314295e52b06a7e6dc2beae31c943fe790
SANJITH THÉ MAGNIFICENT <sanjithkumar986@gmail.com>
2024-03-11 09:11:03 +0530

    back_utils_up:0911

--- Back_Utils.py
+++ Back_Utils.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from src.data_cleaning import *
+from src.data_scaling import *
 import pandas as pd
 from typing import List
 
-# @st.cache_data
+@st.cache_data
 def drop_column(column: List[str], data : pd.DataFrame) -> pd.DataFrame:
 
     drop_data = DataDrop(data)
@@ -11,7 +12,7 @@ def drop_column(column: List[str], data : pd.DataFrame) -> pd.DataFrame:
 
     return data
 
-# @st.cache_data
+@st.cache_data
 def impute_columns(columns : List[str], data : pd.DataFrame, strategy: str, impute_parameters : dict) -> pd.DataFrame:
 
     impute = Imputer(data)
@@ -20,3 +21,19 @@ def impute_columns(columns : List[str], data : pd.DataFrame, strategy: str, impu
         data = impute.handle_data(column, strategy, impute_parameters["strategy"],impute_parameters["fill_value"])
 
     return data
+
+
+@st.cache_data
+def encode(columns :List[str], data : pd.DataFrame, strategy : str):
+    encode = Encoding(data, strategy)
+    for column in columns:
+        data = encode.handle_data(column)
+
+    return data
+
+@st.cache_data
+def standardize(columns: List[str], data : pd.DataFrame, strategy : str):
+
+    standardize = Scaler(data, strategy)
+    for column in columns:
+        data = standardize.handle_data(column)
