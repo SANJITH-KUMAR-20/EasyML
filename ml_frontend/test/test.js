b4f18eb6a91cc4a45a3893409fe417f72195ea77
SANJITH KUMAR R <sanjithkumar986@gmail.com>
2024-03-29 16:29:09 +0530

    test_added

---
+++ ml_frontend/test/test.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var dropColDropdown = document.getElementById("drop-column-dropdown");
+    var dropSelectDropDown = document.getElementById("drop-column-select");
+
+});
