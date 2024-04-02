ec50451f458601f3a21168942a3b94d3870ae323
SANJITH KUMAR R <sanjithkumar986@gmail.com>
2024-04-02 09:31:22 +0530

    test_updated

--- ml_frontend/test/test.js
+++ ml_frontend/test/test.js
@@ -1,27 +1,56 @@
-document.addEventListener("DOMContentLoaded", () => {
-    var dropColDropdown = document.getElementById("drop-column-dropdown");
-    var dropSelectDropDown = document.getElementById("drop-column-select");
-    // dropSelectDropDown.addEventListener("" ,() => {
-        let column = getColumns();
-        column.forEach(col => {
-            var option = document.createElement("option");
-            option.text = col;
-            dropSelectDropDown.appendChild(option);
-        });
-        dropSelectDropDown.style.display = "block";
-
-    // });
-
-});
-
-function getColumns(){
-    var column = [];
-    fetch("http://localhost:8000/get_columns",
-    {
-        method:"GET"
-    }).then(response => response.text()).then(data =>{
-        let columns = data.split(",");
-        columns.forEach(col => column.push(col));
+$(function() {
+  
+    setCheckboxSelectLabels();
+    
+    $('.toggle-next').click(function() {
+      $(this).next('.checkboxes').slideToggle(400);
     });
-    return column;
-}
+    
+    $('.ckkBox').change(function() {
+      toggleCheckedAll(this);
+      setCheckboxSelectLabels(); 
+    });
+    
+  });
+    
+  function setCheckboxSelectLabels(elem) {
+    var wrappers = $('.wrapper'); 
+    $.each( wrappers, function( key, wrapper ) {
+      var checkboxes = $(wrapper).find('.ckkBox');
+      var label = $(wrapper).find('.checkboxes').attr('id');
+      var prevText = '';
+      $.each( checkboxes, function( i, checkbox ) {
+        var button = $(wrapper).find('button');
+        if( $(checkbox).prop('checked') == true) {
+          var text = $(checkbox).next().html();
+          var btnText = prevText + text;
+          var numberOfChecked = $(wrapper).find('input.val:checkbox:checked').length;
+          if(numberOfChecked >= 4) {
+             btnText = numberOfChecked +' '+ label + ' selected';
+          }
+          $(button).text(btnText); 
+          prevText = btnText + ', ';
+        }
+      });
+    });
+  }
+  
+  function toggleCheckedAll(checkbox) {
+    var apply = $(checkbox).closest('.wrapper').find('.apply-selection');
+    apply.fadeIn('slow'); 
+    
+    var val = $(checkbox).closest('.checkboxes').find('.val');
+    var all = $(checkbox).closest('.checkboxes').find('.all');
+    var ckkBox = $(checkbox).closest('.checkboxes').find('.ckkBox');
+  
+    if(!$(ckkBox).is(':checked')) {
+      $(all).prop('checked', true);
+      return;
+    }
+  
+    if( $(checkbox).hasClass('all') ) {
+      $(val).prop('checked', false);
+    } else {
+      $(all).prop('checked', false);
+    }
+  }
