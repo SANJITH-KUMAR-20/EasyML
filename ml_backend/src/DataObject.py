86b739ddf3b99369fbe2aa2ab35a2098170db93d
SANJITH KUMAR R <sanjithkumar986@gmail.com>
2024-05-07 18:35:38 +0530

    dataobj

---
+++ ml_backend/src/DataObject.py
@@ -0,0 +1,55 @@
+from typing import *
+
+class DataObj:
+
+    def __init__(self, data, name):
+
+        """
+        class is used to version a csv data in the form of a pandas dataframe
+
+        Args:
+
+        data -> pd.DataFrame: CSV data
+        name -> str: name of the dataset
+        
+        """
+
+        self.curr_state = data
+        self.prev_states = [] #stores the previous states of the data (versioning part)
+        self.name = name
+
+    def change_state(self, new_state):
+
+        """
+        used to change the current state of the data object
+
+        Args:
+
+        new_state -> pd.DataFrame : New CSV data
+        """
+        self.prev_states.append(self.curr_state)
+        self.curr_state = new_state
+
+    def get_state(self):
+        """
+        Returns the current state of the data
+
+        Returns:
+        curr_state -> pd.DataFrame : CSV data
+        """
+        return self.curr_state
+    
+    def return_prev_state(self):
+        """
+        changes the current state to the previous state
+        """
+        self.curr_state = self.prev_states.pop()
+
+    def get_name(self):
+        """
+        Returns the name of the dataset
+
+        Returns:
+        name -> str : name of the dataset
+        """
+        return self.name
